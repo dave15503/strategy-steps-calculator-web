2556b7021b1e0d638894b11d3721f3ef9c3a2799
dave15503 <davidschuberth@posteo.de>
2025-08-29 00:02:24 +0200

    fixing serve script

--- serve.sh
+++ serve.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Run the server directly from terminal, because setting up docker takes too much time now
-export SS_STATIC_WEBPATH="./website"
+export SS_STATIC_WEBPATH="./frontend/dist"
 export SS_SERVER_PORT=80
 
-python3 ./server.py &
+source venv/bin/activate
+nohup python3 ./server.py &
